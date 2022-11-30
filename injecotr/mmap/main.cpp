85362ab2c6bfd67d97a6c66c996b27495726d6a7
ue4canvas <118943918+ue4canvas@users.noreply.github.com>
2022-11-30 16:08:43 +0900

    proc_handler

---
+++ injecotr/mmap/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "mmap.hpp"
+
+int main(int argc, char **argv) { 
+	mmap kernelMapper;
+
+	LOG("Trying to attach to Overwatch...");
+
+	if (!kernelMapper.attach_to_process("Overwatch.exe"))
+		return 1;
+
+	if (!kernelMapper.load_dll("libow.dll"))
+		return 1;
+
+	if (!kernelMapper.inject())
+		return 1;
+
+	LOG("\nPress any key to close.");
+	static_cast<void>(std::getchar());
+	 
+	return 0;
+}
